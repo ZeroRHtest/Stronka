fae21356f4aa0c5abdd2ce02b03d701c1de53a74
Paul <emaildogry123@gmail.com>
2020-06-18 17:50:48 +0200

    jqueryadd

--- JS/mojsktypt.js
+++ JS/mojsktypt.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
 	// menu click event
 	$('.menu_button').click(function() {
 		$(this).toggleClass('act');
-			if($(this).hasClass('act')) {
-				$('.Menu').addClass('act');
+			
+		if($(this).hasClass('act')) {
+				$('.navigation-list_menu').addClass('act');
 			}
-			else {
-				$('.Menu').removeClass('act');
+			
+		else {
+				$('.navigation-list_menu').removeClass('act');
 			}
 	});
 });
